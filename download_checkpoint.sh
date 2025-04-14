07ddfb8c984eaf078e1def1bff10c64896070f3b
RobbieLiuu <113489661+RobbieLiuu@users.noreply.github.com>
2025-04-14 16:28:59 +0800

    Add files via upload

---
+++ download_checkpoint.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+pip install --upgrade gdown
+
+
+gdown "https://drive.google.com/uc?id=18n1q5RoGU25O0vxaa2qu84d_4Rwvcild&export=download"
+
+if [ -f "checkpoint.pkl" ]; then
+    echo "success download"
+else
+    echo "fail download"
+fi
