e98f42e0d2e8abdc6ab41f76823ed094a5ea8dbf
RobbieLiuu <113489661+RobbieLiuu@users.noreply.github.com>
2025-04-14 16:46:43 +0800

    Update my_td_mcts.py

--- my_td_mcts.py
+++ my_td_mcts.py
@@ -2,7 +2,7 @@ import copy
 import random
 import math
 import numpy as np
-
+from n_tuple_v2 import NTupleApproximator
 # Note: This MCTS implementation is almost identical to the previous one,
 # except for the rollout phase, which now incorporates the approximator.
 
