a62bd749e5be9ae6c656481a1f9043f5fe4e6fda
RobbieLiuu <113489661+RobbieLiuu@users.noreply.github.com>
2025-04-14 16:34:26 +0800

    Update download_checkpoint.sh

--- download_checkpoint.sh
+++ download_checkpoint.sh
@@ -5,8 +5,3 @@ pip install --upgrade gdown
 
 gdown "https://drive.google.com/uc?id=18n1q5RoGU25O0vxaa2qu84d_4Rwvcild&export=download"
 
-if [ -f "checkpoint.pkl" ]; then
-    echo "success download"
-else
-    echo "fail download"
-fi
